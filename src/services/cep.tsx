0985b896c761799dc79c0b3e9d0cb590ae1a8104
diegcarlos <53383018+diegcarlos@users.noreply.github.com>
2020-11-09 09:44:11 -0400

    Update cep.tsx

--- src/services/cep.tsx
+++ src/services/cep.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from 'react';
 import Axios from 'axios'
 
-async function getCep(cep:string, typeData:'json' | 'jsonp' | 'xml' | 'querty') {
-    const { data } = await Axios.get(`http://viacep.com.br/ws/${cep}/${typeData}`);
-    return data;
-
+interface DataCep {
+    bairro: string;
+    cep: string;
+    complemento?: string;
+    ddd: Number;
+    ibge: Number;
+    gia?: string;
+    localidade: string;
+    siafe?: string
+    uf: string
 }
-export default getCep
+
+// async function buscaCep(cep: any, dataType: any) {
+//     const { data } = await Axios(`https://viacep.com.br/ws/${cep}/${dataType}`);
+//     return data;
+// }
+
+const buscaCep = (cep: string, dataType:'json' | 'jsonp' | 'xml' | 'querty') => 
+        Axios(`https://viacep.com.br/ws/${cep}/${dataType}`)
+
+function Cep(cep:string, typeData:'json' | 'jsonp' | 'xml' | 'querty') {
+
+    const [address, setAddress] = useState({});
+
+
+      useEffect(() => {
+        async function fetchData() {
+          const { data }: any = await buscaCep(cep, typeData);
+          setAddress(data);
+
+        }
+        fetchData();
+      }, []);
+      
+    return (address)
+}
+
+export default Cep
